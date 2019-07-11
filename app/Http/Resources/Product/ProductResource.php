5f9cbf7bc0d1b40220ad2d4a081b93e26691eed0
Joshua Olowogbayi <olowogbayigbenga@gmail.com>
2019-07-11 09:38:33 +0100

    Product resource modified

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
           'name' => $this->name,
           'description' => $this->detail,
           'price' => $this->price,
-          'stock' => $this->stock,
-          'discount' => $this->discount
+          'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+          'discount' => $this->discount,
+          'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),
+          'rating' => round($this->reviews->avg('star')) == 0 ? 'No rating yet' : round($this->reviews->avg('star')) ,
+          'href' =>[
+            'reviews' => route('reviews.index',$this->id)
+          ]
         ];
     }
 }
