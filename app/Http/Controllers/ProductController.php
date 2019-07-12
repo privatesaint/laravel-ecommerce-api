17f5337797730193190f70bf15c209ea6243a1bc
Joshua Olowogbayi <olowogbayigbenga@gmail.com>
2019-07-12 09:04:05 +0100

    Create Product added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,9 +6,15 @@ use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
+use App\Http\Requests\ProductRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+  public function __construct()
+  {
+    $this->middleware('auth:api')->except('index','show');
+  }
     /**
      * Display a listing of the resource.
      *
@@ -19,15 +25,6 @@ class ProductController extends Controller
         return ProductCollection::collection(Product::paginate(5));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -35,9 +32,18 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+      $product = new Product;
+      $product->name = $request->name;
+      $product->detail = $request->description;
+      $product->stock = $request->stock;
+      $product->price = $request->price;
+      $product->discount = $request->discount;
+      $product->save();
+        return response([
+          "data" => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
