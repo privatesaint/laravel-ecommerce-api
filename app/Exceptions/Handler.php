59f9e2705d90a83595851d019468760dc538bc3a
Joshua Olowogbayi <47260715+privatesaint@users.noreply.github.com>
2019-07-13 11:44:12 +0100

    Revert "Handles the exception"

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,6 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -49,20 +46,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($request->expectsJson()) {
-            // Model not found exception
-            if ($exception instanceof ModelNotFoundException) {
-                return response()->json([
-                    'errors' => 'Product Model not Found'
-                ],Response::HTTP_NOT_FOUND);
-            }
-            // route not found exception
-            if ($exception instanceof NotFoundHttpException) {
-                return response()->json([
-                    'errors' => 'Incorrect url'
-                ],Response::HTTP_NOT_FOUND);
-            }  
-        }
         return parent::render($request, $exception);
     }
 }
